04bad3a82f0b172097822bc06c666885dc6150c6
laurirasanen <lauri@laurirasanen.com>
2021-02-14 01:02:05 +0200

    Remove debug stuff

--- NoitaMod.Core/Core.cs
+++ NoitaMod.Core/Core.cs
@@ -37,21 +37,9 @@ namespace NoitaMod.Core
             Logger.Instance.WriteLine( "NoitaMod.Core.Init()" );
 
             Process process = Process.GetCurrentProcess();
-            process.EnableRaisingEvents = true;
-            process.Exited += ( sender, e ) =>
-            {
-                Logger.Instance.WriteLine( $"Process exit: {process.ExitCode.ToString()}" );
-                Logger.Instance.Dispose();
-            };
 
             Handles.Init( process.Handle );
             Scanner.Instance.Init( "noita.exe" );
-
-            // Testing
-            var pattern = "6c 69 67 68 74 6e 69 6e 67 5f 63 6f 75 6e 74 00 6d 61 74 65 72 69 61 6c";
-            var addr = Scanner.Instance.FindPattern(pattern);
-            Logger.Instance.WriteLine( $"Pattern: {pattern}" );
-            Logger.Instance.WriteLine( $"Pattern addr: 00x{addr.ToString( "X8" )}" );
         }
     }
 }
