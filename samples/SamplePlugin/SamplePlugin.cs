d455eb6f2a97130ce8b1e09e57894dbbfa9c28c9
laurirasanen <lauri@laurirasanen.com>
2021-02-14 17:18:35 +0200

    Add sample plugin

---
+++ samples/SamplePlugin/SamplePlugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoitaMod.Common;
+
+namespace SamplePlugin
+{
+    public class SamplePlugin : IPlugin
+    {
+        private PluginInfo pluginInfo;
+
+        public PluginInfo PluginInfo
+        {
+            get { return pluginInfo; }
+            set { pluginInfo = value; }
+        }
+
+        public SamplePlugin()
+        {
+            pluginInfo = new PluginInfo()
+            {
+                Name = "SamplePlugin"
+            };
+        }
+
+        public void OnLoad( ILogger logger )
+        {
+            logger.WriteLine( "SamplePlugin.OnLoad()" );
+            logger.WriteLine( "Hello from sample plugin :)" );
+        }
+
+        public void OnUnload()
+        {
+
+        }
+    }
+}
